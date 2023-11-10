de1941bc6dccc15182808c8d496ebbc46a0c32b9
hernping.lee <hernping.lee@gwmail.gwu.edu>
2023-11-10 16:41:56 -0500

    Create Login and Signup page

---
+++ app/src/main/java/com/example/project/LoginActivity.kt
@@ -0,0 +1,118 @@
+package com.example.project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+
+private const val LOG_TAG = "LOGIN PAGE"
+private const val LOG_TAG_FB = "$LOG_TAG FIREBASE"
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var emailInput : EditText
+    private lateinit var passwordInput : EditText
+    private lateinit var rememberMeCheckbox : CheckBox
+    private lateinit var loginBtn : Button
+    private lateinit var signupBtn : Button
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Init
+        emailInput = findViewById(R.id.emailInput)
+        passwordInput = findViewById(R.id.passwordInput)
+        rememberMeCheckbox = findViewById(R.id.rememberMeCheckbox)
+        loginBtn = findViewById(R.id.loginBtn)
+        signupBtn = findViewById(R.id.signupBtn)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val sharedPrefs = getSharedPreferences("MainActivity", MODE_PRIVATE)
+
+        // Email Input
+        emailInput.addTextChangedListener(textWatcher)
+        var savedEmail = intent.getStringExtra("EMAIL")
+        if (savedEmail.isNullOrBlank()) {
+            savedEmail = sharedPrefs.getString("EMAIL", "")
+        }
+        emailInput.setText(savedEmail.toString())
+
+        // Password Input
+        passwordInput.addTextChangedListener(textWatcher)
+        var savedPassword = intent.getStringExtra("PASSWORD")
+        if (savedPassword.isNullOrBlank()) {
+            savedPassword = sharedPrefs.getString("PASSWORD", "")
+        }
+        passwordInput.setText(savedPassword.toString())
+
+        // Checkbox
+        rememberMeCheckbox.isChecked = sharedPrefs.getBoolean("REMEMBERME", false)
+
+        // Login Button
+        loginBtn.isEnabled = emailInput.text.toString().isNotBlank() && passwordInput.text.toString().isNotBlank()
+        loginBtn.setOnClickListener {
+            Log.d(LOG_TAG, "loginBtn clicked")
+            val email : String = emailInput.text.toString().trim()
+            val password : String = passwordInput.text.toString().trim()
+
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Log.d(LOG_TAG_FB, "Login user success")
+
+                        if (rememberMeCheckbox.isChecked) {
+                            Log.d(LOG_TAG, "rememberMe Checked")
+                            sharedPrefs.edit().putString("EMAIL", email).apply()
+                            sharedPrefs.edit().putString("PASSWORD", password).apply()
+                            sharedPrefs.edit().putBoolean("REMEMBERME", true).apply()
+                        } else {
+                            Log.d(LOG_TAG, "rememberMe Unchecked")
+                            sharedPrefs.edit().putString("EMAIL", "").apply()
+                            sharedPrefs.edit().putString("PASSWORD", "").apply()
+                            sharedPrefs.edit().putBoolean("REMEMBERME", false).apply()
+                        }
+                        val user = firebaseAuth.currentUser
+                        Toast.makeText(this, "Login success : ${user!!.displayName}", Toast.LENGTH_LONG).show()
+
+//                        startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
+                    } else {
+                        val exception = it.exception
+                        AlertDialog.Builder(this)
+                            .setTitle("Error")
+                            .setMessage(exception!!.message.toString())
+                            .setPositiveButton("OK", null)
+                            .show()
+                        Log.d(LOG_TAG_FB, exception.toString())
+                    }
+                }
+        }
+
+        // Signup Button
+        signupBtn.setOnClickListener {
+            Log.d(LOG_TAG, "signupBtn clicked")
+            val signupActivity = Intent(this@LoginActivity, SignupActivity::class.java)
+            startActivity(signupActivity)
+        }
+    }
+
+    private val textWatcher : TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            val email : String = emailInput.text.toString()
+            val password : String = passwordInput.text.toString()
+            loginBtn.isEnabled = email.isNotBlank() && password.isNotBlank()
+        }
+
+        override fun afterTextChanged(p0: Editable?) {}
+    }
+}
---
+++ app/src/main/java/com/example/project/SignupActivity.kt
@@ -0,0 +1,121 @@
+package com.example.project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.userProfileChangeRequest
+
+private const val LOG_TAG = "SIGNUP PAGE"
+private const val LOG_TAG_FB = "$LOG_TAG FIREBASE"
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var backBtn : ImageButton
+    private lateinit var emailInput : EditText
+    private lateinit var nameInput : EditText
+    private lateinit var passwordInput : EditText
+    private lateinit var confirmPasswordInput : EditText
+    private lateinit var createAccountBtn : Button
+    private lateinit var errorIcon : ImageView
+    private lateinit var errorText : TextView
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        // Init
+        backBtn = findViewById(R.id.backBtn)
+        emailInput = findViewById(R.id.emailInput)
+        nameInput = findViewById(R.id.nameInput)
+        passwordInput = findViewById(R.id.passwordInput)
+        confirmPasswordInput = findViewById(R.id.confirmPasswordInput)
+        createAccountBtn = findViewById(R.id.createAccountBtn)
+        errorIcon = findViewById(R.id.errorIcon)
+        errorText = findViewById(R.id.errorText)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // TextWatcher
+        emailInput.addTextChangedListener(textWatcher)
+        nameInput.addTextChangedListener(textWatcher)
+        passwordInput.addTextChangedListener(textWatcher)
+        confirmPasswordInput.addTextChangedListener(textWatcher)
+
+        // Back Button
+        backBtn.setOnClickListener {
+            startActivity(Intent(this@SignupActivity, LoginActivity::class.java))
+        }
+
+        // Create Account Button
+        createAccountBtn.setOnClickListener {
+            Log.d(LOG_TAG, "createAccountBtn clicked")
+
+            val email : String = emailInput.text.toString().trim()
+            val password : String = passwordInput.text.toString().trim()
+            val name : String = nameInput.text.toString().trim()
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {task ->
+                    if (task.isSuccessful) {
+                        Log.d(LOG_TAG_FB, "Create user success")
+
+                        firebaseAuth.currentUser!!.updateProfile(userProfileChangeRequest{displayName = name})
+                            .addOnCompleteListener {
+                                if (it.isSuccessful) {
+                                    Log.d(LOG_TAG_FB, "User name updated: $name.")
+                                    Toast.makeText(this, "Created user : ${firebaseAuth.currentUser!!.displayName}", Toast.LENGTH_LONG).show()
+
+                                    val loginActivity = Intent(this@SignupActivity, LoginActivity::class.java)
+                                    loginActivity.putExtra("EMAIL", email).putExtra("PASSWORD", password)
+                                    startActivity(loginActivity)
+                                }
+                            }
+                    } else {
+                        val exception = task.exception
+                        AlertDialog.Builder(this)
+                            .setTitle("Error")
+                            .setMessage(exception!!.message.toString())
+                            .setPositiveButton("OK", null)
+                            .show()
+                        Log.d(LOG_TAG_FB, exception.toString())
+                    }
+                }
+        }
+    }
+
+
+    private val textWatcher : TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            val email : String = emailInput.text.toString()
+            val name : String = nameInput.text.toString()
+            val password : String = passwordInput.text.toString()
+            val confirmPassword : String = confirmPasswordInput.text.toString()
+
+            errorIcon.isVisible = password.isNotBlank() && confirmPassword.isNotBlank() && password != confirmPassword
+            errorText.isVisible = errorIcon.isVisible
+
+            createAccountBtn.isEnabled = email.isNotBlank()
+                                      && name.isNotBlank()
+                                      && password.isNotBlank()
+                                      && confirmPassword.isNotBlank()
+                                      && password == confirmPassword
+        }
+
+        override fun afterTextChanged(p0: Editable?) {}
+    }
+}
